e4a922197a5672a656abd1bdf3b77c7d9b631b36
Nguyễn Trung Hiếu <144864144+TrungHieu24@users.noreply.github.com>
2024-03-26 10:39:54 +0700

    abc

---
+++ HRMS/Data/HRMSContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using HRMS.Models;
+
+namespace HRMS.Data
+{
+    public class HRMSContext : DbContext
+    {
+        public HRMSContext (DbContextOptions<HRMSContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<HRMS.Models.Employee> Employee { get; set; } = default!;
+    }
+}
